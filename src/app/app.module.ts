7b68a4d9b797d954343b01ab540d565cfd6e18da
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-27 21:53:46 -0600

    Created basic page header component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './core/components/header/page-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule,
