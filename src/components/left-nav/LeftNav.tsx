0a6cda043ac457159f6d78d5d9cd8ec3a69cc661
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-28 22:03:26 -0600

    Updated left nav to include basic user-card.

--- src/components/left-nav/LeftNav.tsx
+++ src/components/left-nav/LeftNav.tsx
@@ -1,9 +1,32 @@
+import React from 'react';
 import './LeftNav.scss';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import { CardHeader } from '@mui/material';
 
-export default function LeftNav() {
-    return (
-        <div className="left-nav bg-lush">
-            <span>Hello, world!</span>
-        </div>
-    );
+class LeftNav extends React.Component {
+    seed: number;
+    constructor(props: any) {
+        super(props);
+        this.seed = Math.floor(Math.random() * 9999);
+    }
+    override render() {
+        return (
+            <div className="left-nav bg-lush">
+                <Stack className="user-stack" direction="row" spacing={1}>
+                    <CardHeader className="user-card"
+                        avatar={
+                            <Avatar
+                                alt="Hazel"
+                                src={`https://picsum.photos/200?random=${this.seed}`}
+                            />
+                        }
+                        title="Hazel"
+                    />
+                </Stack>
+            </div>
+        );
+    }
 }
+
+export default LeftNav;
