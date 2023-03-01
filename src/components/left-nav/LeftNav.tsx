3267f398e8c486a63dd17e6595fd3039df77a2dd
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-02-28 21:03:39 -0600

    Added basic elements to left nav.

--- src/components/left-nav/LeftNav.tsx
+++ src/components/left-nav/LeftNav.tsx
@@ -2,6 +2,8 @@ import './LeftNav.scss';
 
 export default function LeftNav() {
     return (
-        <span>Hello, world!</span>
+        <div className="left-nav bg-lush">
+            <span>Hello, world!</span>
+        </div>
     );
 }
