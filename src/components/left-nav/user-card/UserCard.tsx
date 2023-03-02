5a75636541d30d9f1900aee722dd775ae7837fae
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-01 18:24:56 -0600

    Removed 'usestate' import.

--- src/components/left-nav/user-card/UserCard.tsx
+++ src/components/left-nav/user-card/UserCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './UserCard.scss';
 import { Avatar, Space } from 'antd';
 import { UserCardProps } from './UserCardProps';
