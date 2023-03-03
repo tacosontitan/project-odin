6a2f7a1a9dd04d308a7e9f5b302bebb65956b833
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-02 21:10:51 -0600

    Created basic POST endpoint model.

---
+++ src/core/api/PostEndpoint.ts
@@ -0,0 +1,17 @@
+import { ApiEndpoint } from "./ApiEndpoint";
+
+/**
+ * Represents an ApiEndpoint with a POST method.
+ * @extends ApiEndpoint
+ */
+export class PostEndpoint extends ApiEndpoint {
+        /**
+     * Creates a new ApiEndpoint instance.
+     * @param url The URL of the endpoint.
+     * @param headers The headers of the endpoint.
+     * @param body The body of the endpoint.
+     */
+    constructor(url: string, headers: Headers | null = null, body: any = null) {
+        super(url, "POST", headers, null);
+    }
+}
