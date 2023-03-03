0c909d38147d87a2235e61eb06b291734646bdac
Taco (タコス) <65432314+tacosontitan@users.noreply.github.com>
2023-03-02 21:11:18 -0600

    Created basic PUT endpoint model.

---
+++ src/core/api/PutEndpoint.ts
@@ -0,0 +1,17 @@
+import { ApiEndpoint } from "./ApiEndpoint";
+
+/**
+ * Represents an ApiEndpoint with a GET method.
+ * @extends ApiEndpoint
+ */
+export class GetEndpoint extends ApiEndpoint {
+        /**
+     * Creates a new ApiEndpoint instance.
+     * @param url The URL of the endpoint.
+     * @param headers The headers of the endpoint.
+     * @param body The body of the endpoint.
+     */
+    constructor(url: string, headers: Headers | null = null, body: any = null) {
+        super(url, "PUT", headers, null);
+    }
+}
