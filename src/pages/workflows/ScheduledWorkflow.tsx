22e54527088dff92bbc9cf6a4b982e30e20069f8
Sophia <sophia@perzan.dev>
2023-03-11 14:07:49 -0500

    Created files for new ScheduledWorkflow component

---
+++ src/pages/workflows/ScheduledWorkflow.tsx
@@ -0,0 +1,9 @@
+import { ScheduledWorkflowProps } from "./ScheduledWorkflowProps"
+
+export const ScheduledWorkflow: React.FC<ScheduledWorkflowProps> = (props: ScheduledWorkflowProps) => {
+
+    return (
+        <></>
+    );
+
+}
